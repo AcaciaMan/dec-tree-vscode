8f35b2ca7809732372606d9e7619374655d8fbe0
AcaciaMan <11488694+AcaciaMan@users.noreply.github.com>
2024-08-18 08:43:19 +0300

    send only an sent settings

--- dec-tree-vscode/src/m_settings/m_settings.ts
+++ dec-tree-vscode/src/m_settings/m_settings.ts
@@ -1,7 +1,11 @@
+import { PythonScript } from "../python_message";
+
 export class M_Settings {
   private static instance: M_Settings;
 
-  public obj: { [key: string]: string } = {};
+  public sent_obj: { [key: string]: any } = {};
+  public obj: { [key: string]: any } = {};
+  public pythonScript: PythonScript = new PythonScript();
 
   private constructor() {
     // Initialization code here
@@ -14,7 +18,37 @@ export class M_Settings {
     return M_Settings.instance;
   }
 
-  public addSetting( name: string, value: string ): void {
+  public addSetting( name: string, value: any ): void {
     this.obj[name] = value;
   }
+
+  // get pythonScript to send only ansent settings
+    public getPythonScript(): PythonScript {
+        this.pythonScript.imports = [];
+        this.pythonScript.declarations = [];
+        this.pythonScript.code = ["m_result = 'OK'"];
+        this.pythonScript.m_return = "m_result";
+    
+        let new_obj: { [key: string]: any } = {};
+
+        for (const key in this.obj) {
+        if (this.obj.hasOwnProperty(key)) {
+            //make a new object with only the new settings
+            if (this.sent_obj.hasOwnProperty(key)) {
+              if (this.sent_obj[key] !== this.obj[key]) {
+                new_obj[key] = this.obj[key];
+              }
+            } else {
+
+              new_obj[key] = this.obj[key];
+            }
+        }    
+        }
+
+        this.pythonScript.declarations.push({ m_settings: new_obj });
+
+        this.sent_obj = this.obj;
+    
+        return this.pythonScript;
+    }
 }
