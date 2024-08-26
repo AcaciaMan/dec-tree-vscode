2ccd98367918e638ac98697dbbcc93fc2ec07db5
AcaciaMan <11488694+AcaciaMan@users.noreply.github.com>
2024-08-26 11:29:56 +0300

    Updated shop config

--- dec-tree-vscode/src/m_shop/providerShopConfig.ts
+++ dec-tree-vscode/src/m_shop/providerShopConfig.ts
@@ -28,66 +28,31 @@ export class ProviderShopConfig implements vscode.WebviewViewProvider {
 
         webviewView.webview.onDidReceiveMessage((message) => {
             switch (message.command) {
-                case "execute":
-                    vscode.window.showInformationMessage(`Executing: ${message.text}`);
-                    break;
-                case "load":
-                    // Call the __call__ method of the M_Classifier class
-                    // This will load the data and train the model
-                    vscode.window.showInformationMessage(`Loading...`);
-
-                    m_classifier.__call__().then(() => {
-                        vscode.workspace.openTextDocument({ content: M_App.pyApp.channel.received_output }).then((doc) => {
-                            vscode.window.showTextDocument(doc);
-                        });
-                    });
-
-                    // close information message
-                    vscode.window.showInformationMessage(`Loaded!`);
-
-                    break;
-                case "plot":
-                    // Call the plot_tree method of the M_Classifier class
-                    // This will plot the decision tree
-                    vscode.window.showInformationMessage(`Plotting...`);
-
-                    m_classifier.plot_tree().then(async () => {
-                        const filePath = vscode.Uri.file(iset.obj["rootFolder"] + "/decision_tree.png");
-                        await vscode.commands.executeCommand("vscode.open", filePath);
-                    });
-
-                    break;
-                case "importance":
-                    // Call the plot_importance method of the M_Classifier class
-                    // This will plot the feature importance
-                    vscode.window.showInformationMessage(`Plotting importance...`);
-
-                    m_classifier.plot_importance().then(async () => {
-                        const filePath = vscode.Uri.file(iset.obj["rootFolder"] + "/feature_importance.png");
-                        await vscode.commands.executeCommand("vscode.open", filePath);
-                    });
-
-                    break;
                 case "openJson":
                     vscode.window.showInformationMessage(`Opening Json...`);
                     //open message.text that contains json in new tab
 
                     const jsondata = message.text;
                     const filePath = vscode.Uri.file(iset.obj["rootFolder"] + "/shop-config.json");
-                    vscode.workspace.fs.writeFile(filePath,
-                        new TextEncoder().encode(jsondata)
-                    );
+                    //vscode.workspace.fs.writeFile(filePath,
+                    //    new TextEncoder().encode(jsondata)
+                    //);
                     vscode.commands.executeCommand("vscode.open", filePath);
 
                     break;
-                case "saveJson":
-                    vscode.window.showInformationMessage(`Saving Json...`);
-                    break;
                 case "loadJson":
                     vscode.window.showInformationMessage(`Loading Json...`);
-                    break;
-                case "sendJson":
-                    vscode.window.showInformationMessage(`Sending Json...`);
+                    //load json from file shop-config.json
+                    const filePath2 = vscode.Uri.file(iset.obj["rootFolder"] + "/shop-config.json");
+                    vscode.workspace.fs.readFile(filePath2).then(
+                        (data) => {
+                            //const jsondata = new TextDecoder().decode(data);
+                            
+                            //assign data to iset object 'shop-config' 
+                            iset.addSetting("shop-config", JSON.parse(new TextDecoder().decode(data)));
+                            
+                        }
+                    );
                     break;
 
 
@@ -128,44 +93,15 @@ export class ProviderShopConfig implements vscode.WebviewViewProvider {
             <textarea id="jsondata" rows="10" cols="50"></textarea>
             <!-- add posibility to open enetered json data in new tab -->
             <button id="openJson">Open Json</button>
-            <button id="saveJson">Save Json</button>
             <button id="loadJson">Load Json</button>
-            <button id="sendJson">Send Json</button>
-            <br>
-            <button id="executeButton">Execute</button>
-            <br>
-            <button id="loadButton">Load</button>
-            <button id="plotButton">Decision Tree</button>
-            <button id="importanceButton">Feature Importance</button>
             <script nonce="${nonce}">
                 const vscode = acquireVsCodeApi();
-                document.getElementById('executeButton').addEventListener('click', () => {
-                    const inputText = document.getElementById('inputText').value;
-                    vscode.postMessage({ command: 'execute', text: inputText });
-                });
                 document.getElementById('openJson').addEventListener('click', () => {
                     const jsondata = document.getElementById('jsondata').value;
                     vscode.postMessage({ command: 'openJson', text: jsondata });
                 });
-                document.getElementById('saveJson').addEventListener('click', () => {
-                    const jsondata = document.getElementById('jsondata').value;
-                    vscode.postMessage({ command: 'saveJson', text: jsondata });
-                });
                 document.getElementById('loadJson').addEventListener('click', () => {
                     vscode.postMessage({ command: 'loadJson' });
-                });
-                document.getElementById('sendJson').addEventListener('click', () => {
-                    const jsondata = document.getElementById('jsondata').value;
-                    vscode.postMessage({ command: 'sendJson', text: jsondata });
-                });
-                document.getElementById('loadButton').addEventListener('click', () => {
-                    vscode.postMessage({ command: 'load' });
-                });
-                document.getElementById('plotButton').addEventListener('click', () => {
-                    vscode.postMessage({ command: 'plot' });
-                });
-                document.getElementById('importanceButton').addEventListener('click', () => {
-                    vscode.postMessage({ command: 'importance' });
                 });
             </script>
 
